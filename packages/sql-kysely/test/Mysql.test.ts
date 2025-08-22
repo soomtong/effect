7e3c6fb484779529c10e55e6104accdcaa7f5c0c
YoungTaek <soomtong@gmail.com>
2025-08-22 22:44:05 +0900

    Add more Kysely tests with various operations
    The tests now include insert, select, update, and delete operations,
    along with select by ID and limit.

--- packages/sql-kysely/test/Mysql.test.ts
+++ packages/sql-kysely/test/Mysql.test.ts
@@ -31,13 +31,27 @@ describe("MysqlKysely", () => {
 
       yield* db.insertInto("users").values({ name: "Alice" })
       const inserted = yield* db.selectFrom("users").selectAll()
-      yield* db.updateTable("users").set({ name: "Bob", nickname: "The Bobinator" })
-      const updated = yield* db.selectFrom("users").selectAll()
-      yield* db.deleteFrom("users")
+      yield* db.insertInto("users").values({ name: "Queen" })
+      const insertedMore = yield* db.selectFrom("users").selectAll()
+      const selectedOne = yield* db.selectFrom("users").selectAll().where("id", "=", 1)
+      const selectedFirst = yield* db.selectFrom("users").selectAll().limit(1).orderBy("id", "asc")
+      const selectedLast = yield* db.selectFrom("users").selectAll().limit(1).orderBy("id", "desc")
+      yield* db.updateTable("users").set({ name: "Bob", nickname: "The Bobinator" }).where("id", "=", 1)
+      const selectedOneAfterUpdate = yield* db.selectFrom("users").selectAll().where("id", "=", 1)
+      yield* db.deleteFrom("users").where("id", "=", 1)
+      const selectedOneAfterDelete = yield* db.selectFrom("users").selectAll().where("id", "=", 1)
       const deleted = yield* db.selectFrom("users").selectAll()
 
       assert.deepStrictEqual(inserted, [{ id: 1, name: "Alice", nickname: null }])
-      assert.deepStrictEqual(updated, [{ id: 1, name: "Bob", nickname: "The Bobinator" }])
+      assert.deepStrictEqual(insertedMore, [
+        { id: 1, name: "Alice", nickname: null },
+        { id: 2, name: "Queen", nickname: null }
+      ])
+      assert.deepStrictEqual(selectedOne, [{ id: 1, name: "Alice", nickname: null }])
+      assert.deepStrictEqual(selectedFirst, [{ id: 1, name: "Alice", nickname: null }])
+      assert.deepStrictEqual(selectedLast, [{ id: 2, name: "Queen", nickname: null }])
+      assert.deepStrictEqual(selectedOneAfterUpdate, [{ id: 1, name: "Bob", nickname: "The Bobinator" }])
+      assert.deepStrictEqual(selectedOneAfterDelete, [])
       assert.deepStrictEqual(deleted, [])
     }).pipe(Effect.provide(MysqlLive)), { timeout: 120000 })
 })
